f49e51e0510bf4561bb227f4939a360d28f82421
Kamaldev <contact.kamal96@gmail.com>
2024-11-14 12:09:31 +0530

    first commit

---
+++ devops/src/main/java/ci_cd_pipeline_project/devops/App.java
@@ -0,0 +1,13 @@
+package ci_cd_pipeline_project.devops;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
